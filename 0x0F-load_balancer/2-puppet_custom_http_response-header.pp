d467e5082475bb915e67fd5a4333da7a8b775ee1
Emma Navarro <elnavarro55@gmail.com>
2020-08-15 01:14:45 +0000

    Delete file_line resource for 301 and 404 error

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -8,21 +8,6 @@ package {'nginx web server':
   require => Exec['update'],
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School',
-  require => Package['nginx'],
-}
-
-file_line { 'redirect to 301':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
 file_line {'header response':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
