8895b6d09134461baa2042671a4c30378e06aff1
Emma Navarro <elnavarro55@gmail.com>
2020-08-15 00:59:23 +0000

    Change double quotes for header response line 30

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -27,7 +27,7 @@ file_line {'header response':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By "$HOSTNAME";',
+  line    => "add_header X-Served-By "$HOSTNAME";",
   require => Package['nginx'],
 }
 
