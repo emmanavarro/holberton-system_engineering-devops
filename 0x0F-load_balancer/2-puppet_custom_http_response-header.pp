3b77ce360f471c50f5e0e1cacbd97dc1e6f8378a
Emma Navarro <elnavarro55@gmail.com>
2020-08-15 00:54:39 +0000

    Add require attribute to all the resources

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,31 +1,34 @@
 # Creating a custom HTTP header response for the HAproxy
 exec {'update':
-  command => '/usr/bin/sudo apt-get -y update',
+  command => '/usr/bin/apt-get -y update',
 }
 
 package {'nginx web server':
-  ensure => installed,
-  name   => 'nginx'
+  ensure  => installed,
+  require => Exec['update'],
 }
 
 file { '/var/www/html/index.html':
   ensure  => present,
   path    => '/var/www/html/index.html',
-  content => 'Holberton School'
+  content => 'Holberton School',
+  require => Package['nginx'],
 }
 
 file_line { 'redirect to 301':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
 file_line {'header response':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $HOSTNAME;'
+  line   => 'add_header X-Served-By "$HOSTNAME";',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
